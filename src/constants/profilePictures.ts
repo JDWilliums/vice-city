897e90f4ed946ab23ec96b9a002ccab6b5ad844f
kairu <kyle@kairu.media>
2025-03-13 20:04:31 +0000

    new icons

--- src/constants/profilePictures.ts
+++ src/constants/profilePictures.ts
@@ -3,8 +3,20 @@ export const PROFILE_PICTURES = [
   { id: 'lucia', url: '/images/avatars/lucia.png', label: 'Lucia' },
   { id: 'jason', url: '/images/avatars/jason.png', label: 'Jason' },
   { id: 'trevor', url: '/images/avatars/trevor.png', label: 'Trevor' },
-  { id: 'micheal', url: '/images/avatars/micheal.png', label: 'Micheal' },
-  { id: 'Woman1', url: '/images/avatars/woman1.png', label: 'Woman1' },
+  { id: 'trevor2', url: '/images/avatars/trevor2.png', label: 'Trevor 2' },
+  { id: 'michael', url: '/images/avatars/michael.png', label: 'Michael' },
+  { id: 'michael2', url: '/images/avatars/michael2.png', label: 'Michael 2' },
+  { id: 'Woman1', url: '/images/avatars/woman1.png', label: 'Woman 1' },
+  { id: 'chop', url: '/images/avatars/chop.png', label: 'Chop' },
+  { id: 'franklin', url: '/images/avatars/franklin.png', label: 'Franklin' },
+  { id: 'franklin2', url: '/images/avatars/franklin2.png', label: 'Franklin 2' },
+  { id: 'man1', url: '/images/avatars/man1.png', label: 'Man 1' },
+  { id: 'man2', url: '/images/avatars/man2.png', label: 'Man 2' },
+  { id: 'niko', url: '/images/avatars/niko.png', label: 'Niko' },
+  { id: 'sanandreas', url: '/images/avatars/sanandreas.png', label: 'San Andreas' },
+  { id: 'tommy', url: '/images/avatars/tommy.png', label: 'Tommy Vercetti' },
+  { id: 'vicecity', url: '/images/avatars/vicecity.png', label: 'Vice City' },
+  { id: 'vicecity2', url: '/images/avatars/vicecity2.png', label: 'Vice City 2' },
 
 ] as const;
 
