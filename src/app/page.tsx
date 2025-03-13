6a11fbf7e88a86933165f995ff977c3bf7ffdf30
Jack Williams <36612174+JDWilliums@users.noreply.github.com>
2025-03-13 15:37:12 +0000

    updated countdown date

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ import Navbar from '@/components/Navbar';
 import Footer from '@/components/Footer';
 
 // GTA 6 estimated release date (Fall 2025)
-const RELEASE_DATE = new Date('2025-10-01T00:00:00');
+const RELEASE_DATE = new Date('2025-12-01T00:00:00');
 
 // Array of GTA 6 images to cycle through
 const GTA6_IMAGES = [
