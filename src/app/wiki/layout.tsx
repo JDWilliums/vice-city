444cdc5509879f69172628d818ff9fc298692bf5
Jack Williams <36612174+JDWilliums@users.noreply.github.com>
2025-03-19 19:44:26 +0000

    added titles for main static pages

---
+++ src/app/wiki/layout.tsx
@@ -0,0 +1,17 @@
+import { Metadata } from 'next'
+
+export const metadata: Metadata = {
+  title: 'GTA 6 Wiki | vice.city',
+}
+
+export default function WikiLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <div className="min-h-screen">
+      {children}
+    </div>
+  )
+}
