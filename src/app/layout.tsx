1b1a7f4b2852a7bd45f5e1d9be12049677075e1d
Jack Williams <36612174+JDWilliums@users.noreply.github.com>
2025-03-24 01:53:25 +0000

    Reapply "maybe work 2"
    This reverts commit b7150b018552eb1a0f59e2a31cdc895376ce9de7.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import '@/styles/globals.css';
 import AuthWrapper from '@/components/AuthWrapper';
-import GoogleAnalytics from '@/components/GoogleAnalytics';
+
 import { Suspense } from 'react';
 import { SpeedInsights } from '@vercel/speed-insights/next';
 
@@ -24,9 +24,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <Suspense fallback={null}>
-          <GoogleAnalytics />
-        </Suspense>
+        
       </head>
       <body className="bg-dark-bg text-white min-h-screen">
         <AuthWrapper>
